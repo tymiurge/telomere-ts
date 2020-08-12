901fd7fae7e7843b7cd33edfaee638ac3f90ef9d
Tymur Labas <tmrlabas@gmail.com>
2020-08-12 16:21:34 +0300

    comments in template are omitted

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { buildVariablesScope, assemble } from './parser';
 
+const removeComments = (fileContent: string): string => {
+  let result = '';
+  fileContent.split('\n').forEach((line) => {
+    const commentIdx = line.indexOf('//');
+    if (commentIdx === -1) {
+      result += line;
+    } else if (commentIdx !== 0) {
+      const purifiedLine = line.slice(0, commentIdx);
+      if (purifiedLine.trim() !== '') {
+        result += purifiedLine;
+      }
+    }
+  });
+  return result;
+};
+
 export const generate = (jsonTemplate: string): string => {
-  const template = JSON.parse(jsonTemplate);
+  const rawTemplate = removeComments(jsonTemplate);
+  const template = JSON.parse(rawTemplate);
   if (!Object.prototype.hasOwnProperty.call(template, 'data')) {
     throw new Error('No data property in template.');
   }
---
+++ tests/template-comments.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { generate } from '../src';
+
+describe('comments in template test suit', () => {
+  it('whole line comment is omitted', () => {
+    const templateJSON = `{
+      "variable": "value from static scope variable",
+      // this is a comment
+      "data": {
+        // this is another comment
+        "data": "{{ @variable }}"
+      }
+    }`;
+    const expectedJSON = `{
+      "data": "value from static scope variable"
+    }`;
+    const generated = generate(templateJSON);
+    expect(generated.replace(/\s+/g, '')).eq(expectedJSON.replace(/\s+/g, ''));
+  });
+
+  it('part line comment is omitted', () => {
+    const templateJSON = `{
+      "variable": "value from static scope variable",     // this is a comment
+      "data": {
+        "data": "{{ @variable }}"                         // this is another comment
+      }
+    }`;
+    const expectedJSON = `{
+      "data": "value from static scope variable"
+    }`;
+    const generated = generate(templateJSON);
+    expect(generated.replace(/\s+/g, '')).eq(expectedJSON.replace(/\s+/g, ''));
+  });
+});
